96ae347f35cab29f4e0f117a663fe9f6e313a905
eliana-zah <eliana.learnin@gmail.com>
2024-08-09 00:05:32 +0200

    Injected the weather icon and re-organised the variables in order of appearance on the page

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 function refreshWeather(response) {
+  let cityElement = document.querySelector("#city");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let realFeelTemperatureElement = document.querySelector(
     "#real-feel-temperature"
   );
   let realFeelTemperature = response.data.temperature.feels_like;
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windSpeedElement = document.querySelector("#wind-speed");
-  let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
   temperatureElement.innerHTML = Math.round(temperature);
   realFeelTemperatureElement.innerHTML = Math.round(realFeelTemperature);
 }
